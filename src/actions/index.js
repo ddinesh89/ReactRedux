d6d479d05a10b8caf16f7d9741d7e1f16092f47c
ddinesh89 <dinhit15@gmail.com>
2018-07-01 23:18:44 -0700

    Update index.js
    remove api keu

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const API_KEY="656d31c6a0494e6f0a16985a7c758ff1";
+const API_KEY=<API_KEY>;
 const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
